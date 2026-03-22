55cc5e147357cf72eeb5931cfa98d10451078637
Harshith <harshithsai24@gmail.com>
2026-03-22 22:11:32 +0530

    updated the git ignore to send the env file to the git and removed the cors

--- backend/server.py
+++ backend/server.py
@@ -154,9 +154,8 @@ Answer:"""
 app.include_router(api_router)
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_credentials=False,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
